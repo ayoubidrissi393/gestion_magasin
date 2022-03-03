5e9ac12044b7b8d36098ac02a56ba49249eb82aa
ayoubidrissi393 <idrissi.ayoub.solicode@gmail.com>
2022-03-03 16:54:54 +0100

    day :03/03/2022

---
+++ footer.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<title>Footer 07</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+	
+		<link rel="stylesheet" href="css/ionicons.min.css">
+		<link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="style.css">
+  </head>
+<body>
+<footer class="footer-07">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-md-12 text-center">
+						<a href="#" class="logo"><img  src="images/logo.png" class="logo_f"></a>
+						<p class="menu">
+							<a href="#">Home</a>
+							<a href="#">Agent</a>
+							<a href="#">About</a>
+							<a href="#">Listing</a>
+							<a href="#">Blog</a>
+							<a href="#">Contact</a>
+						</p>
+                        <div class="f_paiment">
+                        <div class="r-paiment">
+                            <h4 class="text-white">MODES DE PAIEMENT ET LIVRAISON</h4>
+                            <a href="#"><img class="icon_f" src="images/visa.png" ></a>
+                            <a href="#"><img class="icon_f" src="images/paypal.png" ></a>
+                            <a href="#"><img class="icon_f" src="images/Mastercard.png" ></a>
+                            <!-- <li class="ftco-animate"><a href="#"><img src="images/facbeook.png" ></a></li> -->
+                        </div>
+
+                        <div class="r-social">
+                            <h4 class="text-white">FIND US</h4>
+                            <a href="#"><img class="icon_f" src="images/facebook-f.png" ></a>
+                            <a href="#"><img class="icon_f" src="images/instagram-f.png" ></a>
+                            <!-- <li class="ftco-animate"><a href="#"><img src="images/facbeook.png" ></a></li> -->
+                        </div>
+                    </div>
+			        </div>
+				</div>
+				<div class="row mt-5">
+					<div class="col-md-12 text-center">
+						<p class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+					  Copyright ©<script>document.write(new Date().getFullYear());</script>2022 All rights reserved | This template is made with <i class="ion-ios-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+					  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+					</div>
+				</div>
+			</div>
+		</footer>
+</body>
+</html>
