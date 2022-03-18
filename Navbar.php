30b614e5a2d3debf6615ea4df0751b2b77921e8c
ayoubidrissi393 <92028419+ayoubidrissi393@users.noreply.github.com>
2022-03-18 01:45:01 +0100

    aaa

--- Navbar.php
+++ Navbar.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
     <?php 
         // session_start();
 
@@ -16,6 +17,31 @@
             <li class="nav-item">
               <a class="nav-link" href="javascript:void(0)">SHOP</a>
             </li>
+=======
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="javascript:void(0)"><img id="img_logo" src="images/logo.png" ></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mynavbar">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">SHOP</a>
+        </li>
+      </ul>
+      <li class="nav-item dropdown">
+          <a class="  navbar-brand" href="#" role="button" data-bs-toggle="dropdown">
+          <img class="icon" src="images/accont.png"></a>
+            <!-- <a class="navbar-brand" href="javascript:void(0)"></a> -->
+          <ul class="dropdown-menu bg-dark">
+            <li><a class="dropdown-item text-white" href="Sign_in.php">Sign In</a></li>
+            <li><a class="dropdown-item text-white" href="create.php">Create Compte</a></li>
+            <!-- <li><a class="dropdown-item" href="#"></a></li> -->
+>>>>>>> Stashed changes
           </ul>
           <li class="nav-item dropdown">
               <a class="  navbar-brand" href="#" role="button" data-bs-toggle="dropdown">
